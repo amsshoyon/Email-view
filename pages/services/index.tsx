d561611eb5090e7645f2fa9dd78d5a1bff2aad3a
Abdullah Al Mamun <amsshoyon@gmail.com>
2022-04-14 20:35:45 +0600

    service cards

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -1,24 +1,23 @@
-import { Button, Card, CardActionArea, CardContent, Grid, Typography } from '@mui/material'
+import { Button, Card, CardActionArea, CardActions, CardContent, Grid, Typography } from '@mui/material'
 import { CustomLink } from '@utils/common'
-import Link from 'next/link'
-import React, { useState } from 'react'
+import React from 'react'
 
 const ServiceCard = () => {
 	return (
 		<CustomLink href="/services/32">
 			<Card>
-				<CardActionArea>
-					<CardContent>
-						<Typography gutterBottom variant="h5" component="div">
-							Lizard
-						</Typography>
-						<Typography variant="body2" color="text.secondary" className='mb-4'>
-							Lizards are a widespread group of squamate reptiles, with over 6,000
-							species, ranging across all continents except Antarctica
-						</Typography>
-						<Button size="small" variant='contained' color='info'>Edit</Button>
-					</CardContent>
-				</CardActionArea>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="div">
+						Lizard
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						Lizards are a widespread group of squamate reptiles, with over 6,000
+						species, ranging across all continents except Antarctica
+					</Typography>
+				</CardContent>
+				<CardActions>
+					<Button size="small" variant='contained' color='info'>Edit</Button>
+				</CardActions>
 			</Card>
 		</CustomLink>
 	)
